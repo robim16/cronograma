e03189100d2e8b596ad42c58b4cb8e43c8e45906
robim16 <carteagajimenez@gmail.com>
2023-08-22 12:50:43 -0500

    Add files via upload
    crear e insertar datos db

--- cronograma.sql
+++ cronograma.sql
@@ -14,21 +14,88 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Volcando estructura de base de datos para cronograma
+CREATE DATABASE IF NOT EXISTS `cronograma` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cronograma`;
+
+-- Volcando estructura para tabla cronograma.actividades
+CREATE TABLE IF NOT EXISTS `actividades` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `colaborador_id` bigint unsigned NOT NULL,
+  `estado_id` bigint unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `actividades_colaborador_id_foreign` (`colaborador_id`),
+  KEY `actividades_estado_id_foreign` (`estado_id`),
+  CONSTRAINT `actividades_colaborador_id_foreign` FOREIGN KEY (`colaborador_id`) REFERENCES `colaboradores` (`id`),
+  CONSTRAINT `actividades_estado_id_foreign` FOREIGN KEY (`estado_id`) REFERENCES `estados` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Volcando datos para la tabla cronograma.actividades: ~2 rows (aproximadamente)
 INSERT INTO `actividades` (`id`, `descripcion`, `fecha_inicio`, `fecha_fin`, `colaborador_id`, `estado_id`, `created_at`, `updated_at`) VALUES
 	(1, 'Encuesta de satisfacción con el servicio', '2023-08-20', '2023-08-20', 1, 1, '2023-08-21 23:44:17', '2023-08-21 23:44:17'),
 	(2, 'Visita de seguimiento del proyecto', '2023-08-21', '2023-08-21', 1, 1, '2023-08-22 00:25:10', '2023-08-22 00:25:10');
 
--- Volcando datos para la tabla cronograma.colaboradores: ~0 rows (aproximadamente)
+-- Volcando estructura para tabla cronograma.colaboradores
+CREATE TABLE IF NOT EXISTS `colaboradores` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `identificacion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombres` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellidos` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `colaboradores_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla cronograma.colaboradores: ~1 rows (aproximadamente)
 INSERT INTO `colaboradores` (`id`, `identificacion`, `nombres`, `apellidos`, `direccion`, `telefono`, `email`, `created_at`, `updated_at`) VALUES
 	(1, '1065890328', 'Lorenzo', 'Geliz Cotera', 'Calle 32 N 12-87', '3189000976', 'lorenzogeliz@gmail.com', '2023-08-21 23:35:19', '2023-08-21 23:35:19');
 
--- Volcando datos para la tabla cronograma.estados: ~0 rows (aproximadamente)
+-- Volcando estructura para tabla cronograma.estados
+CREATE TABLE IF NOT EXISTS `estados` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla cronograma.estados: ~2 rows (aproximadamente)
 INSERT INTO `estados` (`id`, `nombre`, `created_at`, `updated_at`) VALUES
 	(1, 'COMPLETADO', NULL, NULL),
 	(2, 'PENDIENTE', NULL, NULL);
 
+-- Volcando estructura para tabla cronograma.failed_jobs
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Volcando datos para la tabla cronograma.failed_jobs: ~0 rows (aproximadamente)
+
+-- Volcando estructura para tabla cronograma.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Volcando datos para la tabla cronograma.migrations: ~6 rows (aproximadamente)
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
@@ -39,9 +106,31 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(5, '2023_08_19_205520_create_estados_table', 2),
 	(6, '2023_08_19_205615_create_actividades_table', 2);
 
+-- Volcando estructura para tabla cronograma.password_resets
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Volcando datos para la tabla cronograma.password_resets: ~0 rows (aproximadamente)
 
--- Volcando datos para la tabla cronograma.users: ~0 rows (aproximadamente)
+-- Volcando estructura para tabla cronograma.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla cronograma.users: ~1 rows (aproximadamente)
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 	(1, 'Lorenzo Antonio', 'lorenzogeliz@gmail.com', NULL, '$2y$10$Z8JH4lrVbQ81mGJDDnCgiu6NVSn/3H2cFQ.Yg6V3Z.VfkMjcNK1LG', NULL, '2023-08-20 02:42:55', '2023-08-20 02:42:55');
 
