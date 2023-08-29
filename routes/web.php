38b0e593b902a86c6368bf19400868396f0ec90b
Carlos Arteaga <desarrollador2@dydsoluciones.net>
2023-08-29 16:35:44 -0500

    vista ruta home

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect()->route('admin');
 });
 
 Auth::routes();
